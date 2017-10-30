cead589ca076524f602ea68f1523a45ac909e6b0
AlvaroRamirezEscribano <piomh@hotmail.com>
2017-10-30 16:21:31 +0100

    Ejercicios Tema1

---
+++ Tema1/Ejercicio7.java
@@ -0,0 +1,18 @@
+/**
+* Tema 1
+* Ejercicio 7
+* Igual que el programa anterior, pero esta vez la pirámide estará hueca 
+* (se debe ver únicamente el contorno hecho con asteriscos).
+*
+* @author Alvaro
+*/
+
+public class Ejercicio7 { // Clase principal 
+	public static void main(String[] args) { 
+		System.out.println("    *");  
+		System.out.println("   * *"); 
+		System.out.println("  *   *"); 
+		System.out.println(" *     *"); 
+		System.out.println("*********"); 									
+	} 
+}
