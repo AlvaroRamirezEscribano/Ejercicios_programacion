761f02a1633ab6afe950e09d87b5859d7744725a
AlvaroRamirezEscribano <piomh@hotmail.com>
2017-10-30 16:12:12 +0100

    Actualizando
    Subiendo ejercicios

---
+++ Tema1/Ejercicio1.java
@@ -0,0 +1,13 @@
+/**
+* Tema 1
+* Ejercicio 1
+* Escribe un programa que muestre tu nombre por pantalla.
+*
+* @author Alvaro
+*/
+
+public class Ejercicio1 { // Clase principal 
+	public static void main(String[] args) { 
+		System.out.print("Alvaro"); 
+	} 
+}
---
+++ Tema1/Ejercicio2.java
@@ -0,0 +1,17 @@
+/**
+* Tema 1
+* Ejercicio 2
+* Modifica el programa anterior para que además se muestre tu dirección 
+* y tu número de teléfono. Asegúrate de que los datos se muestran en 
+* líneas separadas.
+*
+* @author Alvaro
+*/
+
+public class Ejercicio2 { // Clase principal 
+	public static void main(String[] args) { 
+		System.out.println("Alvaro"); 
+		System.out.println("C/ Jaén nº 30"); 
+		System.out.println("617214719"); 
+	} 
+}
